24d1723364a959409debeadbb6d1f328414871b0
elinavalieva <veaufa@mail.ru>
2022-12-15 14:02:01 +0100

    Feat: extend terraform in cloud build

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,29 @@
 variable "project_id" {
   description = "Project ID"
   default     = "gcp-playground-06071995"
+  type        = string
 }
 
 variable "region" {
   description = "Project Region"
   default     = "us-central1"
+  type        = string
 }
 
 variable "github_account" {
   description = "Github Account Reference"
   default     = "ElinaValieva"
+  type        = string
 }
 
 variable "github_project_name" {
   description = "Github Project Name"
   default     = "gcp_playground_system"
+  type        = string
 }
 
 variable "cloud_run_name" {
   description = "Cloud Run Name"
   default     = "gcp-playground-system"
+  type        = string
 }
