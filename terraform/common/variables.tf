407cb5a239e0a229c711d95c3b4053cb588dae77
elinavalieva <veaufa@mail.ru>
2022-12-15 17:56:54 +0100

    Fix: move ci to separate folder

---
+++ terraform/common/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "Project ID"
+  default     = "gcp-playground-06071995"
+  type        = string
+}
+
+variable "zone" {
+  description = "Project Zone"
+  default     = "us-central"
+  type        = string
+}
+
+variable "region" {
+  description = "Project Region"
+  default     = "us-central1"
+  type        = string
+}
