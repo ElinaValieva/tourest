f3271c168465c08b392741fcb8442993747eb1bf
elinavalieva <veaufa@mail.ru>
2022-12-18 20:02:28 +0100

    Feat: extend firestore

---
+++ src/components/Firebase.js
@@ -0,0 +1,16 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBaBiVelTj_vAI0zO-RimrcDKntshbLz6g",
+    authDomain: "gcp-playground-06071995.firebaseapp.com",
+    projectId: "gcp-playground-06071995",
+    storageBucket: "gcp-playground-06071995.appspot.com",
+    messagingSenderId: "253120544678",
+    appId: "1:253120544678:web:2e83db47d66e6d2c639967",
+    measurementId: "G-H5JJ41HSV9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
