a98af69056993d53a1b07544980420d3594b44da
elinavalieva <veaufa@mail.ru>
2022-12-19 14:30:26 +0100

    Feat: uploading to firebase storage

--- src/components/NewTour.js
+++ src/components/NewTour.js
@@ -4,83 +4,46 @@ import db from '../firebase';
 import {v4 as uuidv4} from 'uuid';
 import {getStorage, ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
 
-export function FileUploader() {
+function DateUtil() {
+    let date = new Date();
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-    const storage = getStorage()
-    const [image, setImage] = useState(null)
-    const [visible, setVisible] = useState(true)
-
-    function onImageChange(event) {
-        if (event.target.files && event.target.files[0]) {
-            let img = event.target.files[0];
-            const storageRef = ref(storage, `/files/${img.name}`)
-            const uploadTask = uploadBytesResumable(storageRef, img);
-
-            uploadTask.on(
-                "state_changed",
-                (snapshot) => {
-                    const percent = Math.round(
-                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                    );
-
-                    // update progress
-                    console.log(percent);
-                },
-                (err) => console.log(err),
-                () => {
-                    // download url
-                    getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                        console.log(url);
-                        setVisible(false)
-                        setImage(url)
-                    });
-                }
-            );
-        }
+    Date.prototype.formatTime = function () {
+        let hours = this.getHours();
+        let minutes = this.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        return hours + ':' + minutes + ' ' + ampm;
     }
 
-    return (
-        <div className="banner">
-            <label>
-                {
-                    visible ?
-                        <div style={{padding: '10%'}}>
-                            <h1 className="upload-image">Upload image</h1>
-                        </div> :
-                        <img alt="Select image" src={image}/>
-                }
-                <input type="file" style={{display: 'none'}} name="myImage"
-                       onChange={(event) => onImageChange(event)}/>
-            </label>
-        </div>
-    )
+    Date.prototype.formatDay = function () {
+        return this.getDay() + ' ' + monthNames[this.getMonth()] + ' ' + this.getFullYear();
+    }
+    return date;
 }
 
 export function NewTour() {
 
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
+    const [image, setImage] = useState(null)
     const [loading, setLoading] = useState(false);
+    const [visible, setVisible] = useState(true)
 
+    function onImageChange(event) {
+        if (event.target.files && event.target.files[0]) {
+            setImage(URL.createObjectURL(event.target.files[0]))
+            setVisible(false)
+        }
+    }
+
+    const storage = getStorage()
     const collectionRef = collection(db, 'test');
 
-    async function addBlog() {
-        let date = new Date();
-        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-        Date.prototype.formatTime = function () {
-            let hours = this.getHours();
-            let minutes = this.getMinutes();
-            const ampm = hours >= 12 ? 'PM' : 'AM';
-            hours = hours % 12;
-            hours = hours ? hours : 12;
-            minutes = minutes < 10 ? '0' + minutes : minutes;
-            return hours + ':' + minutes + ' ' + ampm;
-        }
-
-        Date.prototype.formatDay = function () {
-            return this.getDay() + ' ' + monthNames[this.getMonth()] + ' ' + this.getFullYear();
-        }
+    async function createPost(url) {
+        let date = DateUtil();
 
         const textDetail = {
             id: uuidv4(),
@@ -88,7 +51,7 @@ export function NewTour() {
             avatar: 'https://storage.googleapis.com/tourest_bucket_xxx/author-avatar.png',
             publishedDay: date.formatDay(),
             publishedTime: date.formatTime(),
-            source: 'https://storage.googleapis.com/tourest_bucket_xxx/popular-2.jpeg',
+            source: url,
             surname: 'Admin',
             country: 'Malé',
             city: 'Nepal',
@@ -97,20 +60,50 @@ export function NewTour() {
         }
 
         try {
-            setLoading(true);
             await setDoc(doc(collectionRef), textDetail).then(() => {
                 setLoading(false);
             });
         } catch (error) {
             console.error(error);
-
         }
+    }
+
+    function createPostWithSource() {
+        const storageRef = ref(storage, `/files/${image.name}`)
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        setLoading(true)
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                console.log(percent);
+            },
+            (err) => console.log(err),
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                    createPost(url).then((e) => console.log(e))
+                });
+            }
+        );
     }
 
     return (
         <div>
             <div className="banner">
-                <FileUploader/>
+                <div className="banner">
+                    <label>
+                        {
+                            visible ?
+                                <div style={{padding: '10%'}}>
+                                    <h1 className="upload-image">Upload image</h1>
+                                </div> :
+                                <img alt="Select image" src={image}/>
+                        }
+                        <input type="file" style={{display: 'none'}} name="myImage"
+                               onChange={(event) => onImageChange(event)}/>
+                    </label>
+                </div>
             </div>
 
             <div className="blog">
@@ -123,7 +116,7 @@ export function NewTour() {
             <div className="about-content">
                 <button className="btn btn-primary"
                         disabled={loading}
-                        onClick={() => addBlog()}>Share your tour Now
+                        onClick={() => createPostWithSource()}>Share your tour Now
                 </button>
             </div>
         </div>
