5ecb61952cb4c5c8f6a8c88010a2d7e2d7bdfb66
elinavalieva <veaufa@mail.ru>
2022-12-29 23:52:36 +0100

    Fix: cloud function reference

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -21,32 +21,24 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_bucket_object" "object" {
   name   = "function.zip"
   bucket = google_storage_bucket.bucket.name
-  source = "function.zip" # Add path to the zipped function source code
+  source = "functions/function.zip" # Add path to the zipped function source code
 }
 
 resource "google_cloudfunctions2_function" "function" {
   project     = var.PROJECT_ID
-  name        = "firebase"
+  name        = "firebase_function"
   location    = var.REGION
   description = "a new function"
 
+
   build_config {
-    runtime               = "nodejs16"
-    entry_point           = "post" # Set the entry point
+    runtime     = "nodejs16"
+    entry_point = "post" # Set the entry point
     source {
       storage_source {
         bucket = google_storage_bucket.bucket.name
         object = google_storage_bucket_object.object.name
       }
-    }
-
-    environment_variables = {
-      REACT_APP_FIREBASE_API_KEY             = "AIzaSyBaBiVelTj_vAI0zO-RimrcDKntshbLz6g"
-      REACT_APP_FIREBASE_APP_ID              = "1:253120544678:web:2e83db47d66e6d2c639967"
-      REACT_APP_FIREBASE_AUTH_DOMAIN         = "gcp-playground-06071995.firebaseapp.com"
-      REACT_APP_PROJECT_ID                   = "gcp-playground-06071995"
-      REACT_APP_FIREBASE_STORAGE_BUCKET      = "gcp-playground-06071995.appspot.com"
-      REACT_APP_FIREBASE_MESSAGING_SENDER_ID = "253120544678"
     }
   }
 
@@ -54,7 +46,18 @@ resource "google_cloudfunctions2_function" "function" {
     max_instance_count = 1
     available_memory   = "256M"
     timeout_seconds    = 60
+
+    environment_variables = {
+      REACT_APP_FIREBASE_API_KEY             = data.google_firebase_web_app_config.basic.api_key
+      REACT_APP_FIREBASE_APP_ID              = google_firebase_web_app.wild_workouts.app_id
+      REACT_APP_FIREBASE_AUTH_DOMAIN         = data.google_firebase_web_app_config.basic.auth_domain
+      REACT_APP_PROJECT_ID                   = var.PROJECT_ID
+      REACT_APP_FIREBASE_STORAGE_BUCKET      = lookup(data.google_firebase_web_app_config.basic, "storage_bucket", "")
+      REACT_APP_FIREBASE_MESSAGING_SENDER_ID = lookup(data.google_firebase_web_app_config.basic, "messaging_sender_id", "")
+    }
   }
+
+  depends_on = [google_firebase_project.default]
 }
 
 output "function_uri" {
